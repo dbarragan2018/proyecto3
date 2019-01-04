eff16b980c35a06b00e00c7049ce8141d51aeea1
dbarragan2018 <dbarragan.gastrotek@gmail.com>
2019-01-04 16:33:28 -0600

    segundo commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NuevoComponent } from './nuevo/nuevo.component';
+import { ReporteComponent } from './reporte/reporte.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component:HomeComponent},
+  {path: 'nuevo', component:NuevoComponent},
+  {path: 'reportes', component:ReporteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { NuevoComponent } from './nuevo/nuevo.component';
+import { ReporteComponent } from './reporte/reporte.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    NuevoComponent,
+    ReporteComponent
   ],
   imports: [
     BrowserModule,
